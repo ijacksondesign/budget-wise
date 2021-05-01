bb0f90806eed9ba94c5d89d37f7cbf904ec5a43f
Ian Jackson <ijacksondesign@gmail.com>
2021-05-01 11:20:03 -0400

    added skipwating and claim() to server worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,25 +7,16 @@ const FILES_TO_CACHE = [
     "./css/styles.css",
     "./js/idb.js",
     "./js/index.js",
+    './manifest.json',
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
 ];
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { 
-                // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } 
-            else {       
-                // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-});
 
 self.addEventListener('install', function (e) {
     // tells browser to wait until work is complete before terminating worker
@@ -34,7 +25,8 @@ self.addEventListener('install', function (e) {
             console.log('Installing cache : ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
-    )
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', function (e) {
@@ -53,6 +45,25 @@ self.addEventListener('activate', function (e) {
                     return caches.delete(keyList[i]);
                 }
             }))
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { 
+                // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } 
+            else {       
+                // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
         })
     )
 });
